d29727dcea76587d77b23a8742420afef9f6b61a
Batuhan Özcan <batuhan.ozcan@bil.omu.edu.tr>
2021-06-03 02:03:48 +0300

    add the products create, update and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # API definition
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: %i[show create update destroy index]
+      resources :users
       resources :tokens, only: %i[create]
-      resources :products, only: %i[show index]
+      resources :products
     end
   end
 end
