d293d9d05bfab9472d9262e015deebd754d921cd
Batuhan Özcan <batuhan.ozcan@bil.omu.edu.tr>
2021-06-02 23:49:46 +0300

    finish  modeling the product model along with user associations

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy index]
-      resources :tokens, only: [:create]
+      resources :tokens, only: %i[create]
+      resources :products, only: %i[show index]
     end
   end
 end
