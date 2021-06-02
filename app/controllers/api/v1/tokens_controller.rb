9696c2052b55febabb048c665d78d8f9147cbf95
Batuhan Özcan <batuhan.ozcan@bil.omu.edu.tr>
2021-06-02 13:51:05 +0300

    setup tokens controller

---
+++ app/controllers/api/v1/tokens_controller.rb
@@ -0,0 +1,24 @@
+module Api
+  module V1
+    class TokensController < ApplicationController
+
+      def create
+        @user = User.find_by_email(user_params[:email])
+        if @user&.authenticate(user_params[:password])
+          render json: {
+            token: JsonWebToken.encode(user_id: @user.id),
+            email: @user.email
+          }
+        else
+          head :unauthorized
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email, :password)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # API definition
-  namespace :api, defaults: { format: :json } do 
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy index]
+      resources :tokens, only: [:create]
     end
   end
 end
