79da957bd06384d486e5cbd974fe410afdb11289
Batuhan Özcan <batuhan.ozcan@bil.omu.edu.tr>
2021-06-02 14:31:07 +0300

    restrict actions for unauthorized users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class UsersController < ApplicationController
       before_action :set_user, only: %i[show update destroy]
+      before_action :check_owner, only: %i[update destroy]
 
       # GET /users
       def index
@@ -47,6 +48,10 @@ module Api
 
       def set_user
         @user = User.find(params[:id])
+      end
+
+      def check_owner
+        head :forbidden unless @user.id == current_user&.id
       end
     end
   end
