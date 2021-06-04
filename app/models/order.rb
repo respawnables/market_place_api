626ee7ade3dfcd01bd1858973bb91b2eec8a4efa
Batuhan Özcan <batuhan.ozcan@bil.omu.edu.tr>
2021-06-04 13:38:02 +0300

    associates products and orders with a placements model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
+  has_many :placements, dependent: :destroy
+  has_many :products, through: :placements
+  
   belongs_to :user
 
   validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
